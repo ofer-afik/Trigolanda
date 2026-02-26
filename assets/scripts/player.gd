c0be8c1fd230a6570061765466cf5b8d0dd55305
ofer-afik <ofer.afik123@gmail.com>
2026-02-26 19:30:39 +0200

    Finish player controls and start audio

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,25 +1,86 @@
 extends CharacterBody2D
 
+@onready var animplay: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+@export var friction = 5.0
 
+var look_left = load("res://assets/sprites/main_char/char_up_lt.png")
+var look_right = load("res://assets/sprites/main_char/char_up_rt.png")
+
+# Animation states.
+enum State {
+	# Ground states.
+	IDLE,
+	RUNNING,
+
+	# Air states.
+	JUMP_START,
+	IN_AIR,
+	LANDING
+}
+
+var anim_state : State = State.IDLE
+
+# Called from jump_start animation.
+func jump():
+	velocity.y = jump_velocity
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		anim_state = State.IN_AIR
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and (anim_state == State.IDLE or anim_state == State.RUNNING) and is_on_floor():
+		anim_state = State.JUMP_START
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Handle horizontal movement.
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		if anim_state != State.JUMP_START:
+			velocity.x = direction * speed
+		else:
+			velocity.x = 0
+		if anim_state == State.IDLE:
+			anim_state = State.RUNNING
+		if direction < 0:
+			sprite.texture = look_left
+		else:
+			sprite.texture = look_right
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, speed * delta * friction)
+		if anim_state == State.RUNNING:
+			anim_state = State.IDLE
+	
+	# Handle landing.
+	if is_on_floor() and anim_state == State.IN_AIR:
+		anim_state = State.LANDING
+	
+	# Handle animations.
+	match anim_state:
+		State.IDLE:
+			if not animplay.current_animation == "idle":
+				animplay.play("idle")
+		State.RUNNING:
+			if not animplay.current_animation == "run":
+				animplay.play("run")
+		State.JUMP_START:
+			if not animplay.current_animation == "jump_start":
+				animplay.play("jump_start")
+		State.IN_AIR:
+			pass
+		State.LANDING:
+			if not animplay.current_animation == "land":
+				animplay.play("land")
+	
 	move_and_slide()
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "jump_start" and anim_state == State.JUMP_START:
+		anim_state = State.IN_AIR
+	elif anim_name == "land" and anim_state == State.LANDING:
+		anim_state = State.IDLE
