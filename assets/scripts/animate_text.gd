f108a0c21ee24f138504e70ae9629a8e88aece92
ofer-afik <ofer.afik123@gmail.com>
2026-03-17 09:11:40 +0200

    Change name of game

--- assets/scripts/animate_text.gd
+++ assets/scripts/animate_text.gd
@@ -1,11 +1,25 @@
-extends Node
+@tool
+extends RichTextEffect
+class_name AnimateText
 
+var bbcode := "anim"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _process_custom_fx(char_fx: CharFXTransform) -> bool:
+	var speed = 30.0
+	var duration = 5.0
 
+	if char_fx.relative_index < char_fx.elapsed_time * speed:
+		char_fx.visible = true
+	else:
+		char_fx.visible = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if char_fx.relative_index < char_fx.elapsed_time * speed - duration:
+		return true
+
+	var progress = (char_fx.elapsed_time * speed - char_fx.relative_index) / duration
+	progress = clamp(progress, 0.0, 1.0)
+
+	var angle = progress * PI
+	char_fx.offset.y = -sin(angle) * 20
+
+	return true
